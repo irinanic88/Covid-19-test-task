94ecdcff68d4f649afa142f1e0eff74046b0409a
Irina Nicolai <irina.nic.yu@gmail.com>
2022-04-21 19:25:31 +0200

    Add data to Table

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -6,7 +6,6 @@ import { summarySelector } from '../../store/selectors';
 const Table = () => {
     const { loadSummary } = useActions();
     const summary = useSelector(summarySelector);
-    console.log(summary);
 
     useEffect(() => {
         if (summary.length === 0) {
@@ -14,8 +13,39 @@ const Table = () => {
         }
     }, [summary]);
 
+    if (summary.length === 0) {
+        return <div/>
+    }
+
     return (
-        <div>This is Table</div>
+        <table>
+            <thead>
+            <tr>
+                <th>Country</th>
+                <th>Confirmed</th>
+                <th>Active</th>
+                <th>Recovered</th>
+                <th>Deaths</th>
+            </tr>
+            </thead>
+            <tbody>
+            {
+                summary.map((row, index) => {
+                    const { ID, Country, TotalConfirmed, TotalRecovered, TotalDeaths } = row;
+                    const Active = TotalConfirmed - TotalRecovered - TotalDeaths;
+                    return (
+                        <tr key={`${Country}-${index}`}>
+                            <td>{Country}</td>
+                            <td>{TotalConfirmed}</td>
+                            <td>{Active}</td>
+                            <td>{TotalRecovered}</td>
+                            <td>{TotalDeaths}</td>
+                        </tr>
+                    )
+                })
+            }
+            </tbody>
+        </table>
     )
 }
 
