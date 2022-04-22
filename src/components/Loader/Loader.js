b7980d2fc03ebc4b6b58ba68f0a262e53fd91156
Irina Nicolai <irina.nic.yu@gmail.com>
2022-04-22 14:14:45 +0200

    Add test to Loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -4,7 +4,7 @@ import styles from './Loader.module.scss';
 
 const Loader = () => {
     return (
-        <div className={styles.loader}>
+        <div className={styles.loader} data-id="loader">
             <div className={styles.loader__iconContainer}>
                 <LoaderIcon className={styles.loader__icon}/>
             </div>
